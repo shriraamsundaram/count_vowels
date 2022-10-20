45abe39960078b2dfdb961beeb45e2e317abb070
shriraam.sundaram <sriramsundaram91@gmail.com>
2022-10-20 22:27:32 +0100

    Initial file search using nftw

--- main.c
+++ main.c
@@ -8,14 +8,38 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <pthread.h>
+#include <ftw.h>
+#include <stdlib.h>
+#include <string.h>
 #include "count_vowels.h"
 
+#define _XOPEN_SOURCE 500
+
+
+static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
+{
+    char *fileName = "findMe.txt";
+    /* fpath holds the full path of the file from the specified starting directory */
+    if (strstr(fpath, fileName) )
+    {
+        printf("Match found!\n");
+    }
+    return 0;
+}
 
 int main(int argc, char ** argv)
 {
 	printf("Hello World! \n");
 	printf("%s \n", argv[1]);
 
+	/* If a starting directory isn't specified, use the current dir */
+	if (nftw((argc < 2) ? "/" : argv[1], display_info, 20, 0) == -1)
+	{
+		printf("Error could not find file");
+
+	}
+	return 0;
+#if 0
 	pthread_t thread1, thread2;
 
 	uint64_t thread1VowelCount = 0u;
@@ -37,4 +61,5 @@ int main(int argc, char ** argv)
 	printf("Result : ""%" PRIu64 "\n", thread1VowelCount + thread2VowelCount);
 
 	return 0;
+#endif
 }
