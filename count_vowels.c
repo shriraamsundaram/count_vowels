2642f8fcb40e463f5646df33ff04d6d268627473
shriraam.sundaram <sriramsundaram91@gmail.com>
2022-10-22 20:50:45 +0100

    Update verification for the Read size from file. Compare against the requested size.

--- count_vowels.c
+++ count_vowels.c
@@ -13,43 +13,75 @@
 #include "count_vowels.h"
 
 #define E_NOT_OK (-1)
+#define NUM_RETRIES (100)
 
 void* count_vowels(void * args)
 {
+	uint64_t numVowels = 0u;
+
 	COUNT_VOWEL_ThreadArgs_t * arguments = (COUNT_VOWEL_ThreadArgs_t *)args;
 	const char * fileName = arguments->fileName;
 
-	uint64_t numVowels = 0u;
-	char ch;
+	uint64_t sizeToRead = (arguments->endIndex - arguments->startIndex);
+	char * ch = malloc(sizeToRead * sizeof(char));
+
 	int fd = open(fileName, O_RDONLY);
 
 	if(E_NOT_OK != fd)
 	{
+		char * bufPos = ch;
 		lseek(fd, arguments->startIndex, SEEK_SET);
 
-		for(uint64_t i = 0u; i < (arguments->endIndex - arguments->startIndex); i++)
+		uint64_t totalRead = 0u;
+		for(uint64_t i = 0u; i < NUM_RETRIES; i++)
 		{
-			read(fd, &ch, 1);
-			switch(ch)
+			if(sizeToRead > 0u)
 			{
-				case 'a':
-				case 'e':
-				case 'i':
-				case 'o':
-				case 'u':
-				case 'A':
-				case 'E':
-				case 'I':
-				case 'O':
-				case 'U':
-					numVowels++;
-					break;
-				default:
-					break;
+				/*We get the actual read size because this is not guaranteed  that size requested = size read in the man page*/
+				ssize_t actualReadSize = read(fd, bufPos, sizeToRead);
+
+				if(E_NOT_OK != actualReadSize)
+				{
+
+					totalRead += actualReadSize;
+					for(uint64_t j = 0u; j < actualReadSize; j++)
+					{
+						switch(ch[j])
+						{
+							case 'a':
+							case 'e':
+							case 'i':
+							case 'o':
+							case 'u':
+							case 'A':
+							case 'E':
+							case 'I':
+							case 'O':
+							case 'U':
+								numVowels++;
+								break;
+							default:
+								break;
+						}
+					}
+					sizeToRead = sizeToRead - actualReadSize;
+					if(sizeToRead <= 0)
+					{
+						/* We have completed reading so we end here*/
+						break;
+					}
+					else
+					{
+						/*This is because of the fact that the size requested is less than the size read*/
+						lseek(fd, arguments->startIndex + totalRead, SEEK_SET);
+						bufPos = (char *)realloc(ch, sizeToRead * sizeof(char));
+					}
+				}
 			}
 		}
 		close(fd);
 	}
 	*(arguments->result) = numVowels;
+	free(ch);
 	return NULL;
 }
