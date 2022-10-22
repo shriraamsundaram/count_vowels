bb91c6358e714c6d35f187f8bbfb75d0d8ef21f4
shriraam.sundaram <sriramsundaram91@gmail.com>
2022-10-22 05:32:02 +0100

    Update main to establish fileFound

--- main.c
+++ main.c
@@ -18,19 +18,10 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <stdbool.h>
 #include "count_vowels.h"
 
-
-static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
-{
-    char *fileName = "findMe.txt";
-    /* fpath holds the full path of the file from the specified starting directory */
-    if (strstr(fpath, fileName) )
-    {
-        printf("Match found!\n");
-    }
-    return 0;
-}
+static bool fileFound = false;
 
 void pf(const char * fileName)
 {
@@ -38,6 +29,7 @@ void pf(const char * fileName)
 	{
 		printf("call back called \n");
 		printf("Match found!\n");
+		fileFound = true;
 	}
 }
 
@@ -79,40 +71,29 @@ int main(int argc, char ** argv)
 	bfs(pf, argv[1]);
 	return 0;
 
-#if 0
-	/*Remember where we are*/
-	int fd = open(".", O_RDONLY);
-	chdir("/");
-	fchdir(fd);
-	close(fd);
-	/* If a starting directory isn't specified, use the current dir */
-	if (nftw("/home", display_info, 20, 0) == -1)
+
+	if(fileFound)
 	{
-		printf("Error could not find file");
+		pthread_t thread1, thread2;
+
+		uint64_t thread1VowelCount = 0u;
+		uint64_t thread2VowelCount = 0u;
+		int thread1Ret = 0;
+		int thread2Ret = 0;
+
+		/*Create worker threads to complete the job*/
+		thread1Ret = pthread_create( &thread1, NULL, count_vowels, (void *)(&((COUNT_VOWEL_ThreadArgs_t){argv[1], 1, 5,  &thread1VowelCount})));
+		thread2Ret = pthread_create( &thread2, NULL, count_vowels, (void *)(&((COUNT_VOWEL_ThreadArgs_t){argv[1], 6, 11, &thread2VowelCount})));
+
+
+		/*Wait until the worker thread completes its job*/
+		pthread_join(thread1, NULL);
+		pthread_join(thread2, NULL);
+
+		printf("Value of thread 1: ""%" PRIu64 "\n", thread1VowelCount);
+		printf("Value of thread 2: ""%" PRIu64 "\n", thread2VowelCount);
+		printf("Result : ""%" PRIu64 "\n", thread1VowelCount + thread2VowelCount);
 
 	}
-
-
-	pthread_t thread1, thread2;
-
-	uint64_t thread1VowelCount = 0u;
-	uint64_t thread2VowelCount = 0u;
-	int thread1Ret = 0;
-	int thread2Ret = 0;
-
-	/*Create worker threads to complete the job*/
-	thread1Ret = pthread_create( &thread1, NULL, count_vowels, (void *)(&((COUNT_VOWEL_ThreadArgs_t){argv[1], 1, 5,  &thread1VowelCount})));
-	thread2Ret = pthread_create( &thread2, NULL, count_vowels, (void *)(&((COUNT_VOWEL_ThreadArgs_t){argv[1], 6, 11, &thread2VowelCount})));
-
-
-	/*Wait until the worker thread completes its job*/
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-
-	printf("Value of thread 1: ""%" PRIu64 "\n", thread1VowelCount);
-	printf("Value of thread 2: ""%" PRIu64 "\n", thread2VowelCount);
-	printf("Result : ""%" PRIu64 "\n", thread1VowelCount + thread2VowelCount);
-
 	return 0;
-#endif
 }
